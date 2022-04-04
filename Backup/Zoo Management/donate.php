4bcc93e53dabdeb449a95f58c30357d9cb40720b
Mohammed Ahnaf Abid <73640125+MohammedAhnafAbid1931897642@users.noreply.github.com>
2022-04-05 02:26:48 +0600

    Good

---
+++ Backup/Zoo Management/donate.php
@@ -0,0 +1,32 @@
+<?php
+	$amount = $_POST['amount'];
+	$fname = $_POST['fname'];
+  $lname= $_POST['lname'];
+  $phone= $_POST['phone'];
+  $cc= $_POST['cc'];
+  $cvv= $_POST['cvv'];
+  $email= $_POST['email'];
+
+
+	// Database connection
+	$conn = new mysqli('localhost','root','','zoo2');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into donate(amount, fname, lname, phone, cc, cvv, email) values(?, ?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param("isssiis", $amount, $fname, $lname, $phone, $cc, $cvv, $email);
+		$execval = $stmt->execute();
+
+		echo $execval;
+		echo "Donation successful!...";
+    echo "Redirecting in 5 seconds...";
+    echo '<html>
+    <head>
+        <meta http-equiv="refresh" content="5;url=donate.html" />
+    </head>
+    <body>';
+		$stmt->close();
+		$conn->close();
+	}
+?>
